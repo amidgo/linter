41b715d22d28950b927c2640fcf617123c145de3
amidman <mcdvaran@gmail.com>
2025-11-07 16:14:11 +0400

    increment decrement

--- linter.go
+++
@@ -1,9 +0,0 @@
-package main
-
-func main() {
-	complexityTest()
-}
-
-func complexityTest() {
-
-}
---
+++ linter_test.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	var counter int
+
+	counter += 1
+	counter -= 1
+
+	_ = counter
+}
